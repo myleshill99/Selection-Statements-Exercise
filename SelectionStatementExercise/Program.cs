b873c7466b3234ad207c7854db8047a8e97b0d85
Myles Hill <mylehill99@gmail.com>
2023-10-11 17:17:24 -0400

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
-    public class Program
+   class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        //Number Guessing Game 
+        var r = new Random();
+        var favNumber = r.Next(1 - 1000);
+    
+        
+        var userInput = int.Parse(Console.ReadLine());
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            Console.WriteLine("Pick a number from 1- 1000");
+            userInput = int.Parse(Console.ReadLine());
+
+          if (userInput < favNumber)
+         {
+
+          }
+             Console.WriteLine($"(userInput) is too low");
+         {
+            else if (userInput > favNumber) ;
+            }
+            Console.WriteLine($"{userInput} is too high");
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }  while (userInput != favNumber) ;
+      }
+     }
+ }
